378e36472e478c6f6802a898aa01c90e84f005a3
felxx <igorfelipemonteirocontato@gmail.com>
2025-04-24 22:13:18 -0300

    base inicial do projeto implementada

---
+++ lib/config/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Booklog');
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:booklog/config/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
