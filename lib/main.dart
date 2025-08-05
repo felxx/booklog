ba81c909df12ef608e5b437362c62f91f3e6fd13
felxx <igorfelipemonteirocontato@gmail.com>
2025-08-05 14:32:45 -0300

    fix(workflow): add enviroment variable on build

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,19 @@ import 'package:booklog/config/app.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+const supabaseUrl = String.fromEnvironment('SUPABASE_URL');
+const supabaseAnonKey = String.fromEnvironment('SUPABASE_ANON_KEY');
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  if (supabaseUrl.isEmpty || supabaseAnonKey.isEmpty) {
+    throw Exception('Supabase URL or Anon Key not found in environment variables.');
+  }
+
   await Supabase.initialize(
-    url: 'https://apeyaealkxrifyyzojie.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFwZXlhZWFsa3hyaWZ5eXpvamllIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQzOTMzOTcsImV4cCI6MjA2OTk2OTM5N30.7XFrr1vUDxe7Qn3LNAJTAqdr7CWRiTjzSJM43Y7KznU',
+    url: supabaseUrl,
+    anonKey: supabaseAnonKey,
   );
   runApp(const App());
 }
